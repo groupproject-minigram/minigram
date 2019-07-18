a50b3ff49b0b4a40cc13dd1946ce7be83fdfb589
basyri <husain.basyri@gmail.com>
2019-07-18 17:15:54 +0700

    register

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,9 +1,9 @@
-module.exports = function(err,req,res,next){
+module.exports = function (err, req, res, next) {
     console.log('ke error handler');
     console.log(err)
     const status = err.code || 500
     const message = err.message || 'internal server error'
     res.status(status).json({
-        message : message
+        message: message
     })
 }
