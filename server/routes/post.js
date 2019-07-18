a8982f64f83df69935a6ce2a40cec01da847c330
anameilani <meilani.ana@gmail.com>
2019-07-18 21:22:56 +0700

    add function client

--- server/routes/post.js
+++ server/routes/post.js
@@ -5,7 +5,7 @@ const {authorizationPost}= require('../middlewares/authorize')
 const {sendUploadToGCS, multer}= require('../helpers/image')
 
 router.use(authentication)
-router.get('/allpost', postController.allPost)
+router.get('/', postController.allPost)
 router.get('/mypost', postController.myPost)
 router.post('/', multer.single('image'), sendUploadToGCS, postController.create)
 router.patch('/:postId', postController.updatelike )
